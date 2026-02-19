da1c5b33a2bd5509e06b6b07464fcba4b2e2c8d2
Ben van Enckevort <github@benvan.co.uk>
2026-02-19 08:54:29 +0000

    removed src/*

--- src/core/filter/index.ts
+++
@@ -1,3 +0,0 @@
-export { BasicFilterParser } from './basic-parser.js';
-export { BasicSqlFilterRenderer, jsonExtractFieldMapper } from './basic-sql-renderer.js';
-export type { FieldMapper } from './basic-sql-renderer.js';
--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './connector.js';
-export * from './entity.js';
-export * from './filter.js';
-export * from './permissions.js';
